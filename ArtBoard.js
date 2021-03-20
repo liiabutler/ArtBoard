3604694d8d8ea3d37fcbea3c724c485524c6e48c
Liia Butler <liiamb2@illinois.edu>
2021-03-19 23:45:40 -0500

    add basic key event circle draw

--- ArtBoard.js
+++ ArtBoard.js
@@ -1,4 +1,25 @@
 var c = document.getElementById("artCanvas");
 var ctx = c.getContext("2d");
-ctx.font = "30px Arial";
-ctx.fillText("Hello World", 10, 50);
+
+
+
+var rad = 4;
+var xAxis = 0+rad;
+var yAxis = 0+rad;
+
+window.addEventListener("keypress", (event) => {
+  console.log(`key=${event.key}, code=${event.code}`);
+  var ctx = c.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(xAxis, yAxis, rad, 0, 2 * Math.PI, false);
+  ctx.stroke();
+  xAxis += (2 * rad);
+  if (xAxis >= c.width - rad) {
+    yAxis += (2*rad);
+    xAxis = 0 + rad;
+  }
+  //if (yAxis >= c.height - rad) {
+  //  c.height += (2*rad);
+  //}
+
+});
